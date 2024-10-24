32370fc2eaa55937cb6b6c001f07e7fa1eda8bbe
Ева Старыш <e.starish@avto.pro>
2024-10-24 10:28:08 +0300

    remove extra code

--- ConferenceRoomBooking.Services.API/Profiles/MappingProfile.cs
+++ ConferenceRoomBooking.Services.API/Profiles/MappingProfile.cs
@@ -13,32 +13,17 @@ namespace ConferenceRoomBooking.Services.API.Profiles
         public MappingProfile() 
         {
             CreateMap<BookingFilterDto, BookingFilter>().ReverseMap();
+            CreateMap<BookingDto, Booking>().ReverseMap();
             CreateMap<CreateBookingRequestDto, Booking>().ReverseMap();
 
             CreateMap<ConferenceRoomFilterDto, ConferenceRoomFilter>().ReverseMap();
+            CreateMap<ConferenceRoomDto, ConferenceRoom>().ReverseMap();
             CreateMap<CreateConferenceRoomRequestDto, ConferenceRoom>().ReverseMap();
             CreateMap<UpdateConferenceRoomRequestDto, ConferenceRoom>().ReverseMap()
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<ServiceFilterDto, ServiceFilter>().ReverseMap();
-
-
-            /*CreateMap<ConferenceRoom, ConferenceRoomDto>().ReverseMap();
-            CreateMap<ConferenceRoom, CreateConferenceRoomRequestDto>().ReverseMap();
-            CreateMap<UpdateConferenceRoomRequestDto, ConferenceRoom>()
-            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-
-            CreateMap<Service, ServiceDto>().ReverseMap();
-
-            CreateMap<Booking, BookingDto>();
-            CreateMap<Booking, CreateBookingRequestDto>().ReverseMap();
-
-            CreateMap<CreateConferenceRoomRequestDto, ServiceFilterDto>()
-                .ForMember(dest => dest.Guids, opt => opt.MapFrom(src => src.ServiceIds));
-            CreateMap<UpdateConferenceRoomRequestDto, ServiceFilterDto>()
-                .ForMember(dest => dest.Guids, opt => opt.MapFrom(src => src.ServiceIds));
-            CreateMap<CreateBookingRequestDto, ServiceFilterDto>()
-                .ForMember(dest => dest.Guids, opt => opt.MapFrom(src => src.ServiceIds));*/
+            CreateMap<ServiceDto, Service>().ReverseMap();
         }
     }
 }
